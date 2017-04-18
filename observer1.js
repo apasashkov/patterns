6d9ad265fc4e68d58e66e106e0c0ec40acdc69ec
apasashkov <a.pasashkov@gmail.com>
2017-04-18 14:09:27 +0300

    test1

--- observer1.js
+++ observer1.js
@@ -5,7 +5,20 @@ var eventMixin={
     if(!this._eventHandlers[eventName]) {
       this._eventHandlers[eventName]=[];
     };
-    this_eventHandlers
+    this_eventHandlers[eventName].push(handler);
+  },
+
+  off:function(eventName,handler){
+    var handlers=this._eventHandlers && this._eventHandlers[eventName];
+    if(!handlers) return;
+    for(var i=0;i<handlers.length;i++){
+      if(handlers[i]==handler){
+        handlers.splice(i--,1);
+      }
+    }
+  },
+
+  trigger:function(eventName){
 
   }
 }
