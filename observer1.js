c42231246f1b709c26a3ffc52978a48a2797c39c
apasashkov <a.pasashkov@gmail.com>
2017-04-18 13:51:24 +0300

    observer

---
+++ observer1.js
@@ -0,0 +1,11 @@
+var eventMixin={
+
+  on:function(eventName,handler){
+    if(!this._eventHandlers) this._eventHandlers={};
+    if(!this._eventHandlers[eventName]) {
+      this._eventHandlers[eventName]=[];
+    };
+    this_eventHandlers
+
+  }
+}
