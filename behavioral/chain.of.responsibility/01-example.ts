59abc76b41bba86bda5d3a9955ba6082af153d8c
Anton Pasashkov <anton.pasashkov@aio.network>
2025-08-11 23:35:32 +0200

    improve chain of responsibility

--- behavioral/chain.of.responsibility/01-example.ts
+++ behavioral/chain.of.responsibility/01-example.ts
@@ -1,16 +1,64 @@
-interface IHandler {
-  next: IHandler | null;
-  handle(request: any): void;
-  setNext(handler: IHandler): void;
-}
+/*                  CHAIN OF RESPONSIBILITY
+- use when you have a bunch of handlers and you need to run them in specific order
+- you can construct the order in a realtime
+- each handler knows only about the next possible handler
+- each handler can stop processing and break the chain
+*/
 
-class Handler implements IHandler {
-  next: IHandler | null = null;
-  handle(request: any): void {
-    
-  }
 
-  setNext(handler: IHandler): void {
+interface IRequest {
+  type: string;
+};
+
+class Handler {
+  next: Handler | null = null;
+
+  setNext(handler: Handler) {
     this.next = handler;
+    return handler;
+  }
+
+  handle(request: IRequest) {
+    if (this.next) this.next.handle(request);
+    else console.log('cannot process the request')
   }
 }
+
+const request = { type: 'manager'}
+
+class Support extends Handler {
+  handle(request: IRequest): void {
+    if (request.type === 'support') {
+      console.log('handling support request')
+    } else {
+      console.log('support passes request')
+      super.handle(request);
+    }
+  }
+}
+
+class SeniorSupport extends Handler {
+  handle(request: IRequest): void {
+    if (request.type === 'senior-support') {
+      console.log('handling senior support request')
+    } else {
+      console.log('senior-support passes request')
+      super.handle(request);
+    }
+  }
+}
+
+class Manager extends Handler {
+  handle(request: IRequest): void {
+    if (request.type === 'manager') {
+      console.log('handling manager request')
+    } else {
+      super.handle(request);
+    }
+  }
+}
+
+
+const supportHandler = new Support().setNext(new SeniorSupport()).setNext(new Manager());
+
+supportHandler.handle({ type: 'manager'})
